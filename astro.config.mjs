b11d006939d64c7fbcc93b5f046ae8d1255ef726
javlocan <javlpzcntr@gmail.com>
2023-07-14 09:56:39 +0200

    I dont really know what I'm doing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [react()],
   allowImportingTsExtensions: true,
-  adapter: vercel(),
 });
