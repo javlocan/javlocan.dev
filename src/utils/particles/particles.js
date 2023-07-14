6075b588fc51a45880bf34a2d3e1feda9cc5ba56
javlocan <javlpzcntr@gmail.com>
2023-07-14 10:28:56 +0200

    trying to make particles work in prod

--- src/utils/particles/particles.js
+++ src/utils/particles/particles.js
@@ -624,3 +624,25 @@ var Particles = (function (window, document) {
     window.Particles = Particles;
   }
 })();
+
+window.onload = function () {
+  let numberOfParticles = parseInt(window.innerWidth / 15);
+  console.log(numberOfParticles);
+
+  Particles.init({
+    selector: ".sparkles",
+    color: ["#DA0463", "#404B69"],
+    maxParticles: numberOfParticles,
+    connectParticles: true,
+    responsive: [
+      {
+        breakpoint: 800,
+        options: {
+          color: "#00C9B1",
+          maxParticles: 80,
+          connectParticles: false,
+        },
+      },
+    ],
+  });
+};
